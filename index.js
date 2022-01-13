c1ee01fcdec76e25552cbf416ad46620568e5384
Prabhakar Rana <ranakqr@gmail.com>
2022-01-14 01:09:24 +0530

    adding views for ejs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = 8001;
 app.use('/',require('./routes'));   //use express router
+app.set('views engine','ejs');    //set up views engine
+app.set('views', './views');
 app.listen(port, function (err) {
   if (err) {
     console.log("error found", err);
