ef0687468f0e4553f634dffc1ddc8753f7238e2f
Prabhakar Rana <ranakqr@gmail.com>
2022-03-16 09:09:38 +0530

    display post and related users

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,8 +1,17 @@
+const Post = require("../models/post");
 module.exports.home = function (req, res) {
-   //console.log('show cookies');
-   res.cookie('userid',25);
-   console.log(req.cookies);
-    return res.render('home',{
-       title : "Home"
-    });
-}
+  //console.log('show cookies');
+  //res.cookie("userid", 25);
+   Post.find({})
+     .populate("user")
+     .exec(function (err, posts) {
+        if(err){
+           console.log('error found',err);
+        }
+       return res.render("home", {
+         title: "Codeial | Home",
+         posts: posts
+       });
+     });
+  
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const homeController = require("../controllers/home_controllers");
 console.log("router id loaded");
 router.get("/", homeController.home);
 router.use("/users", require("./users"));
+router.use("/posts",require("./posts"));
 //for any furtHer routes, excess from Here
 //router.use("/router_name",require("./router_file"));
+
 module.exports = router;
